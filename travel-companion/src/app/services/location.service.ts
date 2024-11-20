958b31e9b905d0d60c1531df964b6f5fb421af0c
Oisin Hearne <48627086+Oisin-Hearne@users.noreply.github.com>
2024-11-20 22:44:27 +0000

    Implemented Geolocation Service

---
+++ travel-companion/src/app/services/location.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+  constructor() { }
+
+  async getCurrentPosition() {
+    return await Geolocation.getCurrentPosition();
+  }
+}
